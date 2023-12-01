0b7094c79aa3b52f069c8f56960bae54650421e7
Shuhei Yoshida <yshuhei@ele.kindai.ac.jp>
2023-12-01 17:22:33 +0900

    modified:   src/DigitalHolography.jl

--- src/DigitalHolography.jl
+++ src/DigitalHolography.jl
@@ -61,7 +61,7 @@ end
 return object wave extracted by the parallel four-step phase-shifting method (see Ref. 1, 2).
 Using 2x2 pixels with phase differences of ``\\dfrac{\\pi}{2}`` each as units, the object wave is extracted through the four-step phase-shifting method.
 
-> 1. [Y. Awatsuji, M. Sasada, and T. Kubota, "Parallel quasi-phase-shifting digital holography," Appl. Phys. Lett., **85**, 1069–1071 (2004).](https://doi.org/10.1063/1.1777796)
+> 1. [Y. Awatsuji, M. Sasada, and T. Kubota, "Parallel quasi-phase-shifting digital holography," Appl. Phys. Lett., **85**, 1069-1071 (2004).](https://doi.org/10.1063/1.1777796)
 > 2. [Yasuhiro Awatsuji, "Parallel Phase-Shifting Digital Holography," in Bahram Javidi, Enrique Tajahuerce, Pedro Andrés (eds.), *Multi-Dimensional Imaging*, John Wiley & Sons, Ltd, pp. 1-23](https://doi.org/10.1002/9781118705766.ch1)
 """
 function ParallelPSDH(I)
